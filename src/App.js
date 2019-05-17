6d89fbc909c2d0784f01ea972553e612449ebd61
Aylin DeBruyne <adebruyne88@gmail.com>
2019-05-16 21:43:22 -0400

    first data set test  successfully imported into App component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let data = require("./data/mockData.json")
+  console.log(data)
+  console.log(data.companies[0].company_name)
   return (
     <div className="App">
       <h1>High Five
     </h1>
+      <p>{data.companies[0].company_name}</p>
     </div>
   );
 }
