25b593b496e52ecfb86c8b8799b25a47ac17b1de
Aylin DeBruyne <adebruyne88@gmail.com>
2019-05-17 20:26:09 -0400

    adds logic for getting json data into Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
 import '../css/Home.css';
 import CompaniesList from './CompaniesList';
-let data = require("../data/mockData.json")
-console.log(data)
-console.log(data.companies[0].company_name)
+
 
 class Home extends Component {
     constructor(props) {
@@ -13,7 +11,19 @@ class Home extends Component {
         }
     }
 
+    componentDidMount() {
+        let data = require("../data/mockData.json")
+        console.log(data)
+        // console.log(data.companies[0].company_name)
+        this.setState({
+            data: data
+        })
+
+    }
+
+
     render() {
+        console.log(this.state)
         return (
             <div className="home">
                 <p>This is the Home</p>
