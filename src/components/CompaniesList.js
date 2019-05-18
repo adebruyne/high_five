4e2fcec66fb208ba06703a4fbde31a828f329ac9
Aylin DeBruyne <adebruyne88@gmail.com>
2019-05-17 20:14:14 -0400

    adds display component and css for a single company display in CompaniesList

--- src/components/CompaniesList.js
+++ src/components/CompaniesList.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import '../css/CompaniesList.css';
+import CompanyListItem from './CompaniesListItem';
 
 class CompaniesList extends Component {
     render() {
         return (
             <div className="companies-list ">
                 <p>This is the Company List holder</p>
+                <CompanyListItem />
+                <CompanyListItem />
+                <CompanyListItem />
+                <CompanyListItem />
+                <CompanyListItem />
             </div>
         )
     }
---
+++ src/components/CompaniesListItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../css/CompaniesListItem.css';
+
+class CompaniesListItem extends Component {
+    render() {
+        return (
+            <div className="single-company">
+                <p>Single Company</p>
+            </div>
+        )
+    }
+}
+
+export default CompaniesListItem;
