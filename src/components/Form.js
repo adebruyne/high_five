f366dee7f4f9076db334866f3e7bf31133f49f85
Aylin DeBruyne <adebruyne88@gmail.com>
2019-05-19 12:32:43 -0400

    cleans up form component and adds comments

--- src/components/Form.js
+++ src/components/Form.js
@@ -25,12 +25,12 @@ class Form extends Component {
                         placeholder="Company Name"
                         name="companyName"
                         value={this.state.companyName}
-                        onChange={e => this.change(e)} />
+                        onChange={e => this._change(e)} />
                     <input
                         placeholder="Street"
                         name="street"
                         value={this.state.street}
-                        onChange={e => this.change(e)} />
+                        onChange={e => this._change(e)} />
                     <input
                         placeholder="City"
                         name="city"
@@ -40,37 +40,37 @@ class Form extends Component {
                         placeholder="State"
                         name="state"
                         value={this.state.state}
-                        onChange={e => this.change(e)} />
+                        onChange={e => this._change(e)} />
                     <input
                         placeholder="Zip Code"
                         name="zip"
                         value={this.state.zip}
-                        onChange={e => this.change(e)} />
+                        onChange={e => this._change(e)} />
                     <input
                         placeholder="Company Phone"
                         name="companyPhoneNumber"
                         value={this.state.companyPhoneNumber}
-                        onChange={e => this.change(e)} />
+                        onChange={e => this._change(e)} />
                     <input
                         placeholder="Contact Name"
                         name="contactName"
                         value={this.state.contactName}
-                        onChange={e => this.change(e)} />
+                        onChange={e => this._change(e)} />
                     <input
                         placeholder="Email"
                         name="email"
                         value={this.state.email}
-                        onChange={e => this.change(e)} />
+                        onChange={e => this._change(e)} />
                     <input
                         placeholder="Email"
                         name="email"
                         value={this.state.email}
-                        onChange={e => this.change(e)} />
+                        onChange={e => this._change(e)} />
                     <label>Select Status:
                         <select
                             placeholder="Status"
                             name="status"
-                            onChange={e => this.change(e)}
+                            onChange={e => this._change(e)}
                         >
                             <option value="researching">Researching</option>
                             <option value="pending">Pending</option>
@@ -82,27 +82,27 @@ class Form extends Component {
                         placeholder="Current Profit"
                         name="currentProfit"
                         value={this.state.currentProfit}
-                        onChange={e => this.change(e)} />
+                        onChange={e => this._change(e)} />
 
-                    <button onClick={e => this.onSubmit(e)}>Submit</button>
+                    <button onClick={e => this._onSubmit(e)}>Submit</button>
                 </form>
             </div>
         )
     }
 
-    change = (e) => {
+    //handles change event for each input field
+    _change = (e) => {
         console.log(e.target.value)
         this.setState({
             [e.target.name]: e.target.value
         })
     }
 
-
-
-    onSubmit = (e) => {
+    //handles passing form state up to parent component (Home.js) to be added to the list of companies
+    _onSubmit = (e) => {
         e.preventDefault();
         this.props.onSubmit(this.state)
-        console.log(this.state)
+        //clears all fields after submitting 
         this.setState({
             companyName: '',
             street: '',
