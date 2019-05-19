51719397e6c7abf687d3663de6b613ab38bd1bbe
Aylin DeBruyne <adebruyne88@gmail.com>
2019-05-19 10:17:41 -0400

    adds styling to contact details

--- src/components/CompanyContacts.js
+++ src/components/CompanyContacts.js
@@ -6,10 +6,12 @@ class CompanyContacts extends Component {
         console.log(this.props)
         return (
             <div className="company-contacts">
-                <h6>Contact</h6>
-                <p>Name: {this.props.contact.name}</p>
-                <p>Email: {this.props.contact.email}</p>
-                <p>Phone: {this.props.contact.phone_number}</p>
+                <h3 className="company-contacts-header">Contact Details</h3>
+                <p><em>Name</em>: {this.props.contact.name}</p>
+                <hr />
+                <p><em>Email</em>: {this.props.contact.email}</p>
+                <hr />
+                <p><em>Phone</em>: {this.props.contact.phone_number}</p>
             </div>
         )
     }
